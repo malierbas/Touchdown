5378bc241ddfda20a9101a125bc9381831f857b1
Mehmet Ali <63293527+LykaMali@users.noreply.github.com>
2021-10-12 23:05:06 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Ali on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
